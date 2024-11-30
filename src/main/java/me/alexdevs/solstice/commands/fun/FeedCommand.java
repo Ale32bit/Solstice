53b279d2810fa3c60edbfd81ac0c8891d92b0142
Alessandro Proto <alex@alexdevs.me>
2024-11-30 18:15:39 +0100

    Cleanup

--- src/main/java/me/alexdevs/solstice/commands/fun/FeedCommand.java
+++ src/main/java/me/alexdevs/solstice/commands/fun/FeedCommand.java
@@ -5,8 +5,6 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
