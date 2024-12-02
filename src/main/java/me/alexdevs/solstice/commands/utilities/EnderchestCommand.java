be2f4e3b458f546ffc33f123c9f79dd9734d4c5b
Alessandro Proto <alex@alexdevs.me>
2024-12-02 12:40:15 +0100

    Add ability to view other players' enderchests via `/enderchest [player]` command

--- src/main/java/me/alexdevs/solstice/commands/utilities/EnderchestCommand.java
+++ src/main/java/me/alexdevs/solstice/commands/utilities/EnderchestCommand.java
@@ -1,32 +1,53 @@
 package me.alexdevs.solstice.commands.utilities;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 
+import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class EnderchestCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         var rootCommand = literal("enderchest")
                 .requires(Permissions.require("solstice.command.enderchest", 2))
-                .executes(context -> {
-                    var player = context.getSource().getPlayerOrThrow();
-
-                    var enderChestInventory = player.getEnderChestInventory();
-                    player.openHandledScreen(
-                            new SimpleNamedScreenHandlerFactory((syncId, inventory, playerx) ->
-                                    GenericContainerScreenHandler.createGeneric9x3(syncId, inventory, enderChestInventory),
-                                    Text.translatable("container.enderchest")));
-                    player.incrementStat(Stats.OPEN_ENDERCHEST);
-
-                    return 1;
-                });
+                .executes(context -> execute(context, null))
+                .then(argument("player", EntityArgumentType.player())
+                        .requires(Permissions.require("solstice.command.enderchest.others", 3))
+                        .executes(context -> execute(context, EntityArgumentType.getPlayer(context, "player"))));
 
         dispatcher.register(rootCommand);
+    }
+
+    private static int execute(CommandContext<ServerCommandSource> context, @Nullable ServerPlayerEntity target) throws CommandSyntaxException {
+        var source = context.getSource();
+        var player = source.getPlayerOrThrow();
+
+        if (target != null) {
+            if (Permissions.check(target, "solstice.command.enderchest.exempt")) {
+                source.sendFeedback(() -> Text.literal("You cannot open this enderchest because the player is exempt."), false);
+                return 0;
+            }
+        } else {
+            target = player;
+        }
+
+        var enderChestInventory = target.getEnderChestInventory();
+        player.openHandledScreen(
+                new SimpleNamedScreenHandlerFactory((syncId, inventory, playerx) ->
+                        GenericContainerScreenHandler.createGeneric9x3(syncId, inventory, enderChestInventory),
+                        Text.translatable("container.enderchest")));
+        player.incrementStat(Stats.OPEN_ENDERCHEST);
+
+        return 1;
     }
 }
