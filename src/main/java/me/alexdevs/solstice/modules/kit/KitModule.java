9d96d68ebf45b4c8b67e2c2436e2d3fa991d63f0
Alessandro Proto <alex@alexdevs.me>
2024-12-29 16:14:02 +0100

    Add base for kits

--- src/main/java/me/alexdevs/solstice/modules/kit/KitModule.java
+++ src/main/java/me/alexdevs/solstice/modules/kit/KitModule.java
@@ -1,14 +1,30 @@
 package me.alexdevs.solstice.modules.kit;
 
 import me.alexdevs.solstice.Solstice;
+import me.alexdevs.solstice.api.events.SolsticeEvents;
+import me.alexdevs.solstice.api.module.ModuleBase;
+import me.alexdevs.solstice.modules.kit.commands.KitCommand;
+import me.alexdevs.solstice.modules.kit.data.Kit;
 import me.alexdevs.solstice.modules.kit.data.KitPlayerData;
 import me.alexdevs.solstice.modules.kit.data.KitServerData;
 
-public class KitModule {
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class KitModule extends ModuleBase {
     public static final String ID = "kit";
 
     public KitModule() {
+        super(ID);
+
         Solstice.playerData.registerData(ID, KitPlayerData.class, KitPlayerData::new);
         Solstice.serverData.registerData(ID, KitServerData.class, KitServerData::new);
+
+        //commands.add(new KitCommand(this)); // TODO: work in progress
+    }
+
+    public Map<String, Kit> getKits() {
+        return Solstice.serverData.getData(KitServerData.class).kits;
     }
 }
