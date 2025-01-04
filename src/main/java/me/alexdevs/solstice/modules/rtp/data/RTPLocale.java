a04b3efc68f74f5e66803554180c5c751f1dd52d
Alessandro Proto <alex@alexdevs.me>
2025-01-04 12:13:39 +0100

    Clarify how world permissions work with `/spawn` and `/rtp`

--- src/main/java/me/alexdevs/solstice/modules/rtp/data/RTPLocale.java
+++ src/main/java/me/alexdevs/solstice/modules/rtp/data/RTPLocale.java
@@ -8,6 +8,7 @@ public class RTPLocale {
             Map.entry("timeout", "<gold>Could not find a valid spot in a timely manner.</gold>"),
             Map.entry("tooManyAttempts", "<gold>Too many failed attempts at locating a valid spot.</gold>"),
             Map.entry("unsafe", "<gold>Could not place you in a safe spot.</gold>"),
-            Map.entry("success", "<green>Teleporting to a random location...</green>")
+            Map.entry("success", "<green>Teleporting to a random location...</green>"),
+            Map.entry("noWorldPermission", "<gold>You do not have permission to run this command in this world.</gold>")
     );
 }
