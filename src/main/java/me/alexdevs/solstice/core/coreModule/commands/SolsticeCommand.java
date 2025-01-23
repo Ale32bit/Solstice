921bb459505d8e175edac19c4161ef9ec199e658
Alessandro Proto <alex@alexdevs.me>
2025-01-23 02:13:28 +0100

    Cleanup

--- src/main/java/me/alexdevs/solstice/core/coreModule/commands/SolsticeCommand.java
+++ src/main/java/me/alexdevs/solstice/core/coreModule/commands/SolsticeCommand.java
@@ -7,7 +7,6 @@ import me.alexdevs.solstice.api.events.SolsticeEvents;
 import me.alexdevs.solstice.api.module.Debug;
 import me.alexdevs.solstice.api.module.ModCommand;
 import me.alexdevs.solstice.core.coreModule.CoreModule;
-import me.alexdevs.solstice.modules.customName.CustomNameModule;
 import me.alexdevs.solstice.api.text.Format;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.ServerCommandSource;
