2ccd051c1bd4a292ec85505ac2d29eaeb041a8ec
Alessandro Proto <alex@alexdevs.me>
2025-03-24 19:06:52 +0100

    Change Mixin method to be equal to 1.21.1

--- src/main/java/me/alexdevs/solstice/mixin/events/CommandEventsMixin.java
+++ src/main/java/me/alexdevs/solstice/mixin/events/CommandEventsMixin.java
@@ -2,6 +2,7 @@ package me.alexdevs.solstice.mixin.events;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.ParseResults;
+import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.alexdevs.solstice.api.events.CommandEvents;
 import net.minecraft.commands.CommandSourceStack;
@@ -12,16 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CommandDispatcher.class)
 public class CommandEventsMixin<S> {
-    @Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At("HEAD"), remap = false, cancellable = true)
-    public void execute(final ParseResults<S> parse, CallbackInfoReturnable<Integer> cir) throws CommandSyntaxException {
-        var context = parse.getContext();
-        if (context.getSource() instanceof CommandSourceStack source) {
-            var command = parse.getReader().getString();
-            if (!CommandEvents.ALLOW_COMMAND.invoker().allowCommand(source, command)) {
-                cir.setReturnValue(0);
+    @Inject(method = "parse(Lcom/mojang/brigadier/StringReader;Ljava/lang/Object;)Lcom/mojang/brigadier/ParseResults;", at = @At("HEAD"), remap = false, cancellable = true)
+    public void execute(StringReader reader, S source, CallbackInfoReturnable<ParseResults<S>> cir) throws CommandSyntaxException {
+        if (source instanceof CommandSourceStack stack) {
+            var command = reader.getString();
+            if (!CommandEvents.ALLOW_COMMAND.invoker().allowCommand(stack, command)) {
+                cir.cancel();
             }
 
-            CommandEvents.COMMAND.invoker().onCommand(source, command);
+            CommandEvents.COMMAND.invoker().onCommand(stack, command);
         }
     }
 }
