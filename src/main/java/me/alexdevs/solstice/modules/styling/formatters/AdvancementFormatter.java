52fd11d43f89f299fb69294dae1552a866dbea34
scmcgowen <scmcgowen@gmail.com>
2025-04-12 13:04:44 -0700

    Fix advancement rendering on 1.20.1
    Some modded advancements don't follow the vanilla format

--- src/main/java/me/alexdevs/solstice/modules/styling/formatters/AdvancementFormatter.java
+++ src/main/java/me/alexdevs/solstice/modules/styling/formatters/AdvancementFormatter.java
@@ -3,6 +3,7 @@ package me.alexdevs.solstice.modules.styling.formatters;
 import eu.pb4.placeholders.api.PlaceholderContext;
 import me.alexdevs.solstice.Solstice;
 import me.alexdevs.solstice.modules.styling.StylingModule;
+import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.FrameType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
@@ -10,10 +11,11 @@ import me.alexdevs.solstice.api.text.Format;
 import java.util.Map;
 
 public class AdvancementFormatter {
-    public static Component getText(ServerPlayer player, String advancementKey, String frameId) {
-        var frame = FrameType.byName(frameId);
-        var title = advancementKey + ".title";
-        var description = advancementKey + ".description";
+    public static Component getText(ServerPlayer player, Advancement advancement) {
+        var frame = advancement.getDisplay().getFrame();
+        var frameId = frame.getName();
+        var title = advancement.getDisplay().getTitle().getString();
+        var description = advancement.getDisplay().getDescription().getString();
 
         var config = Solstice.modules.getModule(StylingModule.class).getConfig();
 
