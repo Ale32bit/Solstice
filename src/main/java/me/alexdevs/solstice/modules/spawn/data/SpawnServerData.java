0ab700658c3f29282f7826e509c7d7259b075636
Alessandro Proto <alex@alexdevs.me>
2025-04-24 22:48:54 +0200

    Fix spawn setting not working in other dimensions

--- src/main/java/me/alexdevs/solstice/modules/spawn/data/SpawnServerData.java
+++ src/main/java/me/alexdevs/solstice/modules/spawn/data/SpawnServerData.java
@@ -3,9 +3,14 @@ package me.alexdevs.solstice.modules.spawn.data;
 import me.alexdevs.solstice.api.ServerLocation;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SpawnServerData {
     @Deprecated
     public @Nullable ServerLocation spawn;
 
     public @Nullable ServerLocation firstSpawn;
+
+    public Map<String, ServerLocation> spawnPoints = new HashMap<>();
 }
