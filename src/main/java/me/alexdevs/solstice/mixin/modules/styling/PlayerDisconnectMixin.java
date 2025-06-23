129cd6fed79c9ae1580bff376d0f978135c00317
Alessandro Proto <alex@alexdevs.me>
2025-06-23 14:25:08 +0200

    Make join/leave messages silent to those with the permission `solstice.chat.activity.silent`. (#51)
    Closes #50.

--- src/main/java/me/alexdevs/solstice/mixin/modules/styling/PlayerDisconnectMixin.java
+++ src/main/java/me/alexdevs/solstice/mixin/modules/styling/PlayerDisconnectMixin.java
@@ -1,21 +1,24 @@
 package me.alexdevs.solstice.mixin.modules.styling;
 
+import me.alexdevs.solstice.modules.styling.StylingModule;
 import me.alexdevs.solstice.modules.styling.formatters.ConnectionActivityFormatter;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class PlayerDisconnectMixin {
     @Shadow
     public ServerPlayer player;
 
-    @ModifyArg(method = "removePlayerFromWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Z)V"))
-    private Component solstice$getPlayerLeaveMessage(Component message) {
-        return ConnectionActivityFormatter.onLeave(this.player);
+    @Redirect(method = "removePlayerFromWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Z)V"))
+    private void solstice$sendLeaveMessage(PlayerList playerList, Component message, boolean bypassHiddenChat) {
+        var formattedMessage = ConnectionActivityFormatter.onLeave(this.player);
+        StylingModule.broadcastActivity(playerList, formattedMessage, bypassHiddenChat);
     }
 }
