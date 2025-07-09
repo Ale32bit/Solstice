ce224e21d13a4322778083e9468aabf966ed0d10
Alessandro Proto <alex@alexdevs.me>
2025-07-09 21:02:29 +0200

    Bugfix command spy not working

--- src/main/java/me/alexdevs/solstice/mixin/events/CommandEventsMixin.java
+++ src/main/java/me/alexdevs/solstice/mixin/events/CommandEventsMixin.java
@@ -2,7 +2,7 @@ package me.alexdevs.solstice.mixin.events;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.ParseResults;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.StringReader;
 import me.alexdevs.solstice.api.events.CommandEvents;
 import net.minecraft.commands.CommandSourceStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,9 +10,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CommandDispatcher.class)
+@Mixin(value = CommandDispatcher.class, remap = false)
 public abstract class CommandEventsMixin<S> {
-    @Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At("HEAD"), remap = false)
+    // Lcom/mojang/brigadier/CommandDispatcher;parse(Lcom/mojang/brigadier/StringReader;Ljava/lang/Object;)Lcom/mojang/brigadier/ParseResults;
+
+    @Inject(method = "parse(Lcom/mojang/brigadier/StringReader;Ljava/lang/Object;)Lcom/mojang/brigadier/ParseResults;", at = @At("HEAD"), cancellable = true)
+    public void solstice$interceptCommands(StringReader reader, S sourceGeneric, CallbackInfoReturnable<ParseResults<S>> cir) {
+        if (sourceGeneric instanceof CommandSourceStack source) {
+            var command = reader.getString();
+            if (!CommandEvents.ALLOW_COMMAND.invoker().allowCommand(source, command)) {
+                cir.cancel();
+            }
+
+            CommandEvents.COMMAND.invoker().onCommand(source, command);
+        }
+    }
+
+    // This isn't working anymore...?
+    /*@Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At("HEAD"), remap = false)
     public void execute(ParseResults<S> parse, CallbackInfoReturnable<Integer> cir) throws CommandSyntaxException {
         var context = parse.getContext();
         if (context.getSource() instanceof CommandSourceStack source) {
@@ -23,5 +38,5 @@ public abstract class CommandEventsMixin<S> {
 
             CommandEvents.COMMAND.invoker().onCommand(source, command);
         }
-    }
+    }*/
 }
